60c9505215d7b15be73ef6b5b9750946c936d9af
wonchenqin <23247166@student.uwa.edu.au>
2022-09-10 17:03:42 +0800

    Update main.py
    Added in corrections to class methods

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Solver:
 
     #reads input from the user to use as parameters
     #function blocks until a valid input is given
-    def read_user_input():
+    def read_user_input(self):
         #accept user input through the terminal on the function to use, and the
         #x bounds of the function
         #Up to you to figure out how to store the function, and how to pass it to the
@@ -20,14 +20,14 @@ class Solver:
 
     #accepts the function and bounds parameters, executing calculations to determine the corresponding 4bar linkage
     #returns an integer array, giving the lengths of the linkages
-    def execute_4bar_calculations():
+    def execute_4bar_calculations(self):
         #How the parameters are taken depends on how the above function is done
         #also deal with and report negative error checking
         print()
 
     #prints out the results of the calculations
     #takes the integer array of lengths as parameter
-    def print_results():
+    def print_results(self):
         #print the results out to the terminal
         #can draw a graph or some other fancy shit too
         print()
