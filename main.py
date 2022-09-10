1cc2af8896c4d0dfd07ee28a0a9780a8d0157a70
neutronito <vg.sinalco@gmail.com>
2022-09-10 16:09:47 +0800

    Filled out skeleton code

---
+++ main.py
@@ -0,0 +1,34 @@
+#####################################################
+##                   Functions                      #
+#####################################################
+
+#reads input from the user to use as parameters
+#function blocks until a valid input is given
+def read_user_input():
+    #accept user input through the terminal on the function to use, and the
+    #x bounds of the function
+    #Up to you to figure out how to store the function, and how to pass it to the
+    #next functions
+    print()
+
+#accepts the function and bounds parameters, executing calculations to determine the corresponding 4bar linkage
+#returns an integer array, giving the lengths of the linkages
+def execute_4bar_calculations():
+    #How the parameters are taken depends on how the above function is done
+    print()
+
+#prints out the results of the calculations
+#takes the integer array of lengths as parameter
+def print_results():
+    #print the results out to the terminal
+    #can draw a graph or some other fancy shit too
+    print()
+
+    
+
+#####################################################
+##                Main Execution                    #
+#####################################################
+read_user_input()
+execute_4bar_calculations()
+print_results()
