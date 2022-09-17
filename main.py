af99470fb5000dc2bc9bb0b45043101cdd77629e
wonchenqin <23247166@student.uwa.edu.au>
2022-09-17 19:02:10 +0800

    Updated main.py
    Wrote some of the calculation functions, as well as the code to parse expressions from a string.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import scipy
+from sympy.abc import x
+import sympy.parsing.sympy_parser as parser
+import math
 
 #####################################################
 ##                 Solver Class                     #
@@ -6,16 +8,19 @@ import scipy
 class Solver:
     #Class constructor which can take in optional arguments (default to 0)
     #This can be used to construct a default class without needing manual inputs later on
-    def __init__(self, func=0, x_min=0, x_max=0,
+    def __init__(self, func="2*x", x_min=1, x_max=2,
                     theta2_start=0, theta2_max_rot=0,
                     theta4_start=0, theta4_max_rot=0):
-        self.func = func    
+        self.func = parser.parse_expr(func)
         self.x_min = x_min
         self.x_max = x_max
         self.theta2_start   = theta2_start
-        self.theta2_max_rot = theta2_max_rot
+        self.theta2_max_rot = theta2_max_rot       
         self.theta4_start   = theta4_start
         self.theta4_max_rot = theta4_max_rot
+        
+        self.x_points = [0, 0, 0, 0]  # Array to store precision points found given Chebyshev spacing
+        self.y_points = [0, 0, 0]        # Array to store corresponding y-points from above x-points
 
     #reads input from the user to use as parameters
     #function blocks until a valid input is given
@@ -31,8 +36,39 @@ class Solver:
     def execute_4bar_calculations(self):
         #How the parameters are taken depends on how the above function is done
         #also deal with and report negative error checking
+        
+        self.find_chebyshev_spacing()
+        self.find_corresponding_y_points()
         print()
-
+        
+    # Applies Chebyshev spacing formula to determine 3 precision points
+    def find_chebyshev_spacing(self):
+        n = 3
+        self.x_points[3] = self.x_max
+        
+        for j in range(2, -1, -1):   # Counts backwards in sequence 2, 1, 0
+            cos_term = math.cos((2*(j+1) - 1) * math.pi / (2*n))
+            first_term = 0.5 * (self.x_points[j+1] + self.x_min)
+            second_term = 0.5 * (self.x_points[j+1] - self.x_min) * cos_term
+            result = first_term - second_term
+            self.x_points[j] = result 
+        
+        print("Given lower bound of x0 = %f, and upper bound of x4 = %f" % (self.x_min, self.x_max))
+        print("Following precision points found:")
+        print("x1=%f, x2=%f, x3=%f" % tuple(self.x_points[0:3]))
+        print()
+        return
+    
+    def find_corresponding_y_points(self):        
+        for i in range(0, 3):
+            x_val = self.x_points[i]
+            self.y_points[i] = self.func.subs(x, x_val)
+            
+        print("Following corresponding y-points found:")
+        print("y1=%f, y2=%f, y3=%f" % tuple(self.y_points[0:3]))
+        print()
+        return
+        
     #prints out the results of the calculations
     #takes the integer array of lengths as parameter
     def print_results(self):
@@ -45,6 +81,11 @@ class Solver:
     def solve(self, manual=False):
         if manual: 
             self.read_user_input()
+            
+        print("Function entered is:")
+        print("y = " + str(self.func))
+        print()
+        
         self.execute_4bar_calculations()
         self.print_results()
 
@@ -55,4 +96,9 @@ if __name__ == "__main__":
     testSolver = Solver()
     testSolver.solve()
 
-    # testSolver.solve(True)  #Manual input
+    
+    # Examples of input
+    # testSolver = Solver(func="x**(0.5)+9")        # How to enter a function from script - ALWAYS INCLUDE MULTIPLICATION SIGNS
+    
+    
+    # testSolver.solve(manual = True)  # Enter True argument for manual input
